547c743397fe9b50fad026dd04b6c713eef30119
Julie Kraft <julieschmoo@gmail.com>
2014-02-26 12:15:32 -0500

    create rails app and adjusted database.yml

---
+++ bludkin/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bludkin::Application.config.secret_key_base = '7ec76747c8bcf4f52b7ec60d8b413c3db5243e164cddba842e99666b22380561ed99826acd02380d366acb1cf393c1e26b77ffaec63e9c293a23dfcc280cd86d'
