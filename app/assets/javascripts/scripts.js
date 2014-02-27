4c2cf9990aee7b0de820ee77a0bdaa7fbaff0708
Julie Kraft <julieschmoo@gmail.com>
2014-02-27 12:39:01 -0500

    highlighting days on calendar

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,9 +1,26 @@
+var calendar_options = {
+  // put your options and callbacks here
+  dayClick: function(date, allDay, jsEvent, view) {
+
+  // Cycle.new({user_id: this.user.user_id, start_date: date});
+
+  //UGLY, FIX THIS
+  $(this).css('background-color', 'red');
+  $(this).next().css('background-color', 'red');
+  $(this).next().next().css('background-color', 'red');
+  $(this).next().next().next().css('background-color', 'red');
+  }
+  // eventRender: function(event, element) {
+  //     element.qtip({
+  //         content: event.description
+  //     })
+  // }
+}
+
 $(document).ready(function() {
 
-    // page is now ready, initialize the calendar...
+  // page is now ready, initialize the calendar...
+  $('#calendar').fullCalendar(calendar_options)
 
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
 
 });
