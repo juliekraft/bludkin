b256396ea5d1e6f28070f2ae7c5abd1face35937
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-02-27 11:12:36 -0500

    added calendar and js files, fixed fb oauth

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore-min
+//= require backbone
+//= require fullcalendar
+//= require scripts
 //= require turbolinks
-//= require_tree .
+
