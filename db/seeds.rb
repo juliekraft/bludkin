d053a3040c394f328d9935831e50a83ce72dce4b
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-02-27 11:17:41 -0500

    added calendar and js files, fixed fb oauth

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+User.delete_all
 Follow.delete_all
 Message.delete_all
 Cycle.delete_all
 CycleContent.delete_all
 
-follows = Follow.create([{}])
+
+users = User.create([{ email: "sue@email.com", name: "Sue"}, {email: "jo@email.com", name: "JO"}, {email: "julie@email.com", name: "Julie"}, {email: "meghann@email.com", name: "Meghann"} ])
+
+follows = Follow.create([{ follower_id: User.first, followee_id: User.last}])
+
+cycle = Cycle.create([{ start_date: Time.now, period_end_date: Date.new(2014,02,15), cycle_end_date: Date.new(2014, 02, 20), user_id: User.first}, {start_date: Time.now, period_end_date: Date.new(2014,01,15), cycle_end_date: Date.new(2014, 01, 20), user_id: User.first}])
+
+# messages = Message.create({to_id: 1, from_id: 2, message: "I love bleeding"}, {to_id: 2, from_id: 1, message: "I love bloods."}, {to_id: 4, from_id: 3, message: "Bloods is badass"})
