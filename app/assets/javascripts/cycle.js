55afd031ccde74c95913b2b90761581bd72f037c
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-03-01 21:16:01 -0500

    added style to search page

--- app/assets/javascripts/cycle.js
+++ app/assets/javascripts/cycle.js
@@ -148,6 +148,15 @@ UI.Home = Backbone.View.extend({
 
 UI.Friends = Backbone.View.extend({
   initialize: function(){
+    $.getJSON("http://localhost:3000/home/friend.json", function(data){
+    console.log(data)
+    data.forEach(function(user){ 
+      var source = $('#friends-template').html();
+      var template = Handlebars.compile(source);
+      var $user = $(template(user))
+      $('#main-container').append($user)
+    })
+  })
 
   },
   template: function(attributes){
--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -4,20 +4,10 @@
 
 
 
-// THIS GIVES FRIEND PAGE
-$(function(){
-   $('#main-container').empty() //just for testing!
-  $.getJSON("http://localhost:3000/home/friend.json", function(data){
-    console.log(data)
-    data.forEach(function(user){ 
-      var source = $('#friends-template').html();
-      var template = Handlebars.compile(source);
-      var $user = $(template(user))
-      $('#main-container').append($user)
-    })
-    
+// // THIS GIVES FRIEND PAGE
+// $(function(){
+//    $('#main-container').empty() //just for testing!
   
-  })
 
 
-})
+// })
